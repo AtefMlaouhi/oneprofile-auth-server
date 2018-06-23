0fc7622db3e271477c9e216bdd92dbd60023e06d
Kong To <newlight77@gmail.com>
2018-06-24 00:05:23 +0200

    update demo docker-compose parameters

---
+++ demo/docker/config/client/run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+
+cd /demo
+
+
+CP_PARAM="-cp /demo/jars/*"
+CP_PARAM="$CP_PARAM:/demo/config/"
+DEBUG_PARAM="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=$1"
+LAUNCHER_CLASS="org.springframework.boot.loader.JarLauncher"
+SPRING_PARAM="--spring.config.location=file:/demo/config/application-override.yml"
+SPRING_PARAM="$SPRING_PARAM --jwt.publickey.path=/demo/config/key.pub"
+
+
+JAVA_CMD="java $DEBUG_PARAM $CP_PARAM $LAUNCHER_CLASS $SPRING_PARAM"
+
+nohup $JAVA_CMD
